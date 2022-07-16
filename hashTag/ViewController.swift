83063a9a71f4f4a7c159557b32b88ea0939f408a
Reza Kashkoul <rezakashkoul@gmail.com>
2022-07-16 19:49:51 +0430

    Added some part of UI including pan gersture.

--- hashTag/ViewController.swift
+++ hashTag/ViewController.swift
@@ -8,12 +8,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textField?.delegate = self
+        addTextField()
+        setupUI()
+        setupPanGesture()
     }
-
-
+    
+    func setupUI() {
+        title = "Hashtaged!"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        view.backgroundColor = UIColor.lightGray
+    }
+    
+    func addTextField() {
+        let centerScreenWidth = UIScreen.main.bounds.width/2
+        let centerScreenHeight = UIScreen.main.bounds.height/2
+        let textFieldHeight: CGFloat = 60
+        let textFieldWidth: CGFloat = 250
+        textField =  UITextField(frame: CGRect(x: centerScreenWidth-textFieldWidth/2, y: centerScreenHeight-textFieldHeight/2, width: textFieldWidth, height: textFieldHeight))
+        textField.placeholder = "#"
+        textField.borderStyle = .roundedRect
+        textField.font = UIFont.systemFont(ofSize: 40)
+        textField.textAlignment = .center
+        textField.borderStyle = UITextField.BorderStyle.roundedRect
+        textField.autocorrectionType = UITextAutocorrectionType.no
+        textField.keyboardType = UIKeyboardType.default
+        textField.returnKeyType = UIReturnKeyType.done
+        textField.clearButtonMode = UITextField.ViewMode.whileEditing
+        textField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
+        textField.addTarget(self, action: #selector(textFieldDidChange(sender:)), for: .editingChanged)
+        view.addSubview(textField)
+    }
+    
+    fileprivate func setupPanGesture() {
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(self.panGesture))
+        self.textField.addGestureRecognizer(panGesture)
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:))))
+    }
+    
+    @objc func panGesture(sender: UIPanGestureRecognizer){
+        let point = sender.location(in: view)
+        let panGesture = sender.view
+        panGesture?.center = point
+        print(point)
+        textField.resignFirstResponder()
+    }
+    
 }
 
+extension ViewController: UITextFieldDelegate {
+    
+    @objc func textFieldDidChange(sender: UITextField) {
+        print("hiii")
+    }
+    
+}
